fbc7a464610418b049705b22b159cf7f48ee7c5b
greeshmav26 <greeshmav196@gmail.com>
2026-06-08 13:01:28 +0530

    Day 5:Added redraw all and list objects functions

--- graphics_editor.c
+++ graphics_editor.c
@@ -12,10 +12,8 @@
 
 char canvas[ROWS][COLS];
 
-/* Object types */
 typedef enum { CIRCLE, RECTANGLE, LINE, TRIANGLE } ShapeType;
 
-/* Object structure */
 typedef struct {
     ShapeType type;
     int x1, y1, x2, y2;
@@ -87,7 +85,6 @@ void draw_triangle(int r1, int c1, int r2, int c2, int r3, int c3) {
     draw_line(r3, c3, r1, c1);
 }
 
-/* NEW Day 4: Add object to the list */
 void add_object(void) {
     if (obj_count >= MAX_OBJECTS) {
         printf("Canvas full!\n");
@@ -98,7 +95,6 @@ void add_object(void) {
     int choice;
     printf("\nShape: 1=Circle 2=Rectangle 3=Line 4=Triangle: ");
     scanf("%d", &choice);
-
     switch (choice) {
         case 1:
             o->type = CIRCLE;
@@ -132,7 +128,6 @@ void add_object(void) {
     printf("Object %d added successfully!\n", obj_count);
 }
 
-/* NEW Day 4: Delete object from the list */
 void delete_object(void) {
     if (obj_count == 0) {
         printf("No objects to delete!\n");
@@ -149,16 +144,64 @@ void delete_object(void) {
     printf("Object %d deleted!\n", id);
 }
 
+/* NEW Day 5: Redraw all active objects */
+void redraw_all(void) {
+    clear_canvas();
+    for (int i = 0; i < obj_count; i++) {
+        if (!objects[i].active) continue;
+        Object *o = &objects[i];
+        switch (o->type) {
+            case CIRCLE:
+                draw_circle(o->x1, o->y1, o->radius);
+                break;
+            case RECTANGLE:
+                draw_rectangle(o->x1, o->y1, o->x2, o->y2);
+                break;
+            case LINE:
+                draw_line(o->x1, o->y1, o->x2, o->y2);
+                break;
+            case TRIANGLE:
+                draw_triangle(o->x1, o->y1, o->x2, o->y2,
+                              o->radius, o->x2 + 5);
+                break;
+        }
+    }
+}
+
+/* NEW Day 5: List all objects */
+void list_objects(void) {
+    if (obj_count == 0) {
+        printf("No objects yet!\n");
+        return;
+    }
+    const char *names[] = {"Circle","Rectangle","Line","Triangle"};
+    printf("\nID  Shape      Active\n");
+    printf("--- ---------- ------\n");
+    for (int i = 0; i < obj_count; i++)
+        printf("%-3d %-10s %s\n", i + 1,
+               names[objects[i].type],
+               objects[i].active ? "Yes" : "No");
+}
+
 int main(void) {
     clear_canvas();
 
-    /* Test add_object manually */
-    printf("=== Testing Add Object ===\n");
+    /* Test: add a rectangle */
+    printf("=== Add Object ===\n");
     add_object();
 
-    printf("\n=== Testing Delete Object ===\n");
-    delete_object();
+    /* Test: add a circle */
+    printf("=== Add Another Object ===\n");
+    add_object();
 
+    /* Test: list objects */
+    printf("\n=== List Objects ===\n");
+    list_objects();
+
+    /* Test: redraw all */
+    printf("\n=== Redraw All ===\n");
+    redraw_all();
     display_canvas();
+
     return 0;
 }
